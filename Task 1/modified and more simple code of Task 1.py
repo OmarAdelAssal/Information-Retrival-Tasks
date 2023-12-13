0c6ad6a9c4e68941e47b874ed1fb392d3ad8311f
Omar Assal <117770253+OmarAdelAssal@users.noreply.github.com>
2023-12-13 19:09:57 +0200

    Update modified and more simple code of Task 1.py

--- Task 1/modified and more simple code of Task 1.py
+++ Task 1/modified and more simple code of Task 1.py
@@ -5,7 +5,7 @@ inp_Word = input("Enter Word: ")
 inverted_index = []
 
 for i in range(1, 5):
-    file = open(rf"Your Path\{i}.txt", "r")
+    file = open(rf"file path\{i}.txt", "r")
     file_data = file.read()
     tokenized_list = word_tokenize(file_data)
     if inp_Word in tokenized_list:
